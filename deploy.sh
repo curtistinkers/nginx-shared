6d9b0999fa3599a556950e5b11401d7dc0967af7
curtistinkers <curtistinkers.commit@gmail.com>
2025-03-18 00:19:19 +0000

    added user messages and nginx.service reload

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Copying configuration files"
 cp -R conf.d/   /etc/nginx
 cp -R snippets/ /etc/nginx
 cp -R params/   /etc/nginx
+
+echo "Reloading nginx.service"
+systemctl reload nginx.service
