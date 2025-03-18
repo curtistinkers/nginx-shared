cad86048d0bc6dbfc824f1510e8163c4175d4560
curtistinkers <curtistinkers.commit@gmail.com>
2025-03-18 15:55:03 -0500

    check for dhparams and copy if necessary

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-echo "Copying configuration files"
+echo "Copying config files"
 sudo cp -R conf.d/   /etc/nginx
+
+echo "Copying snippets"
 sudo cp -R snippets/ /etc/nginx
+
+echo "Copying proxy paramters"
 sudo cp -R params/   /etc/nginx
+
+echo "Checking for Diffie-Hellman parameters files"
+
+if ! [ -f "/etc/ssl/certs/ffdhe2048.pem" ]; then
+  echo "2048-bit DH parameters file does not exist."
+
+  echo "Copying file from dhparam/ffdhe2048.pem to /etc/ssl/certs/ffdhe2048.pem"
+  cp ./dhparam/ffdhe2048.pem /etc/ssl/certs/ffdhe2048.pem
+else
+  echo "2048-bit DH parameters file already exists."
+fi
+
+if ! [ -f "/etc/ssl/certs/ffdhe4096.pem" ]; then
+  echo "4096-bit DH parameters file does not exist."
+
+  echo "Copying file from dhparam/ffdhe4096.pem to /etc/ssl/certs/ffdhe4096.pem"
+  cp ./dhparam/ffdhe4096.pem /etc/ssl/certs/ffdhe4096.pem
+else
+  echo "4096-bit DH parameters file already exists."
+fi
 
 echo "Validating configuration"
 sudo nginx -t
